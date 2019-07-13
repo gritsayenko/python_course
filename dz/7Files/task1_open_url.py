f2c9f1a35d2c3a433bb46739c1d8dc5a866f9b47
slavicoder <43097476+slavicoder@users.noreply.github.com>
2019-07-13 14:15:37 +0300

    task1 update

--- dz/7Files/task1_open_url.py
+++ dz/7Files/task1_open_url.py
@@ -1,13 +1,13 @@
 import zipfile
-import os
  
-files = zipfile.ZipFile('D:\\Python\\dz\\7Files\\testfile.zip')
-files.extractall('D:\\Python\\dz\\testfile')
+files = zipfile.ZipFile('.\\testfile.zip')
+files.extractall('.\\testfile')
 files.close()
-f = open('D:\\Python\\dz\\testfile\\testfile.dat', 'r')
-for line in f.readlines():
-	if "http" in line:
-		print(line)
+f = open('.\\testfile\\testfile.dat', 'rb')
+while True:
+	data = f.read(1024*1024)
+	if "http" in str(data):
+		print(data)
+	if data == "":
 		break
 f.close()
-  
